5572b91ae2086f12b577e6f48fa0ffb52d0b7a73
Martin Matthesen <mazterarius@gmail.com>
2014-01-08 23:12:45 +0100

    v0.002 winrate.py - Added todo

--- winrate.py
+++ winrate.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+''' 
+
+v0.002 08.01.2014 - Added timestamp memo and added TODO
+v0.001 07.01.2014 - Initial
+
+Upcomming usage of timestamp "+DATE: %s %a %b %d %H:%M:%S %Y" 
+
+TODO:
+
+- Lese file
+- Skrive fil 
+- Kalkulere winrate
+- Quest, Arena
+- Førstegangskjøring
+
+
+''' 
+
 import os.path
 import sys
 
