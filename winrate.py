ca329819c669591c06e6c18170e4dedacfd7b11b
Martin Matthesen <mazterarius@gmail.com>
2014-01-09 19:12:45 +0100

    v0.004 Added check for 'record'

--- winrate.py
+++ winrate.py
@@ -3,6 +3,7 @@
 
 ''' 
 
+v0.004 09.01.2014 - Added a check for "record". Doesn't do anything more with it. Considering standardizing the commandline usage.
 v0.003 09.01.2014 - Added classes as tuple
 v0.002 08.01.2014 - Added timestamp memo and added TODO
 v0.001 07.01.2014 - Initial
@@ -51,9 +52,42 @@ def record():
     print locals().keys()
     
 for arg in sys.argv: 
-    print arg
+    print 'Alle: ' + str(sys.argv[:])
     
-    if arg == 'record': print 'record funnet, vi må kalle på noen!'
+    if len(sys.argv[:]) <= 1: 
+        print 'Ingen argumenter mottatt'
+        break
+    
+    if sys.argv[1] == 'record': 
+        ok = 0
+        print 'record funnet, vi må kalle på noen! Først, sjekke om vi har ord 2, 3 og 4, samt at det er gyldig klasse og argument (4, W|L)'
+
+        print sys.argv[2] + ' <- klasse 1'
+        if sys.argv[2] in klasser:  
+            print 'ok klasse 1'
+            ok += 1
+        
+        print sys.argv[3] + ' <- klasse 2'
+        if sys.argv[3] in klasser:  
+            print 'ok klasse 2'
+            ok += 1
+
+        print sys.argv[4] + ' <- argument'
+        if sys.argv[4] in 'WL':  
+            print 'ok for WL'
+            ok += 1
+
+        print "Sjekk: " + str(ok)
+        if ok == 3: 
+            print 'Alt ok, klasser og resultat er her'
+        else:
+            print 'IKKE OK!!!'
+            break
+        
+        
+        break
+    
+    # Finne andre og tredje ordet
     
     print locals().keys()
     
